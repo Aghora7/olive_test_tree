1f7239cd468ba36a1c2d6a49313a3794966336d1
Aghora7 <ukikieran@gmail.com>
2021-03-14 09:32:07 +0530

    Olivewood: added common lunch choices

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,2 +1,7 @@
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_olive.mk
+    $(LOCAL_DIR)/lineage_olivewood.mk
+
+COMMON_LUNCH_CHOICES := \
+    lineage_olivewood-user \
+    lineage_olivewood-userdebug \
+    lineage_olivewood-eng
